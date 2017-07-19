5caadf2e042db1bb251f1586082d0c326d8ec97c
Lincoln <xzc524813657@sina.com>
2017-07-19 16:55:51 +0800

    'update'

--- routes/Login.js
+++ routes/Login.js
@@ -27,7 +27,6 @@ router.post('/login', function(req, res, next){
     values.push(user.username);
     values.push(password);
     db.findByCondition(selectSql, values, function(result){
-        console.log(result);
         if(result.ResultCode == 1000)
             res.redirect('/api/index');
         else
@@ -35,4 +34,24 @@ router.post('/login', function(req, res, next){
     })
 });
 
+router.get('/register', function(req, res, next){
+    res.render('register',{'Message':'','ResultCode':1000,'Data':[]});
+
+});
+
+router.post('/register', function(req, res, next){
+    var user = req.body;
+    if(user == null)
+        return res.render('login',{'ResultCode':1000,'Message':'用户不能为空',Data:[]});
+    if(user.username == null || user.username =='')
+        return res.render('login',{'ResultCode':1000,'Message':'用户名不能为空',Data:[]});
+    if(user.password == null || user.password =='')
+        return res.render('login',{'ResultCode':1000,'Message':'密码不能为空',Data:[]});
+    var insertSql = 'insert into f_user values(?,?,?,?)';
+    var values = [];
+    values.push(user.username);
+    db.insert(insertSql,values,function(e){
+
+    })
+});
 module.exports = router;
